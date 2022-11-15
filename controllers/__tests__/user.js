f8620c880d8105a2399286c54be0c91e298a5c5e
Salvador Aceves <salvador@aceves.mx>
2022-11-15 10:29:02 +0300

    typo

--- controllers/__tests__/user.js
+++ controllers/__tests__/user.js
@@ -168,7 +168,7 @@ describe('user', () => {
     });
     expect(returnValue.settings.custom).toBe(true);
   });
-  it('testing the user token should include the user app seting', async () => {
+  it('testing the user token should include the user app setting', async () => {
     const { valid } = await doApiPost({
       url: `/${appName}/${userId}/validate`,
       token: userKey,
