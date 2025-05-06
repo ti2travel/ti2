e051c19d31e5ffec890957dda8f8449462ef8ea5
Salvador Aceves <salvador@aceves.mx>
2025-05-06 13:08:12 +0900

    wip: still not a successfull call

--- worker/index.js
+++ worker/index.js
@@ -23,14 +23,67 @@ const worker = ({ plugins: pluginsParam }) => (id, disconnect) => {
   queue.process(maxJobsPerWorker, async job => {
     const {
       data: {
+        type = 'plugin',
         pluginName,
         method,
         userId,
         inTesting,
+        payload = {}
       },
       id: jobId,
       data: params,
     } = job;
+    if (type === 'api') {
+      console.log('job params', params)
+      const {
+        method,
+        url,
+        payload,
+        headers,
+      } = params;
+      const request = require('supertest');
+      // Initialize the app properly
+      const appReq = require('../index');
+      const app = await appReq({
+        startServer: false,
+        plugins: {}
+      });
+      console.log({method, url, payload, headers})
+      const response = await request(app)[method.toLowerCase()](url)
+        .set(headers)
+        .send(payload);
+      const code = response.statusCode;
+      const result = response.body;
+      console.log('job result', result)
+      return {
+        code,
+        result,
+        success: code === 200,
+       };
+    }
+    // Handle callback jobs
+    if (type === 'callback') {
+      const {
+        callbackUrl,
+        operationId,
+        operationPayload,
+        operationResult,
+        integrationId,
+        hint
+      } = payload;
+
+      console.log(`job ${jobId} > sending callback to ${callbackUrl}`);
+      await require('../lib/callback').sendCallback({
+        callbackUrl,
+        operationId,
+        payload: operationPayload,
+        result: operationResult,
+        userId,
+        integrationId,
+        hint
+      });
+      return { success: true };
+    }
     console.log(`job ${jobId} > running ${pluginName}:${method} for ${userId}`);
     const plugins = await (async () => {
       if (inTesting) {
@@ -41,8 +94,10 @@ const worker = ({ plugins: pluginsParam }) => (id, disconnect) => {
     })();
     const thePlugin = plugins.find(({ name }) => name === pluginName);
     const resultValue = await thePlugin[method]({
-      ...params,
+      ...params.payload,
+      token: params.token,
       plugins,
+      typeDefsAndQueries: require('../controllers/bookings').typeDefsAndQueries,
     });
     console.log(`job ${jobId} > completed`);
     await saveResult({ id: jobId, resultValue });
