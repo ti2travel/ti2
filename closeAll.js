05b0f155c5f0f131e7287b78aea5bdf5291d0e1c
Salvador Aceves <salvador@aceves.mx>
2025-05-07 11:57:44 +0900

    Revert "close database connections after running every test"
    This reverts commit b767d714199e4c21d349efd695ab4aa88434dbf3.

--- closeAll.js
+++ closeAll.js
@@ -1,25 +1,8 @@
 const { redisResults, queue } = require('./worker/queue');
 const { cache } = require('./cache');
-const { sequelize } = require('./models');
 
 module.exports = async () => {
-  try {
-    // Close database connection first
-    if (sequelize && typeof sequelize.close === 'function') {
-      await sequelize.close();
-    }
-    
-    // Then close Redis connections
-    if (queue && typeof queue.close === 'function') {
-      await queue.close();
-    }
-    if (redisResults && typeof redisResults.quit === 'function') {
-      await redisResults.quit();
-    }
-    if (cache && typeof cache.quit === 'function') {
-      await cache.quit();
-    }
-  } catch (error) {
-    console.error('Error closing connections:', error);
-  }
+  await queue.close();
+  await redisResults.quit();
+  await cache.quit();
 };
