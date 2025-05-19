02b33803b961011b4c316e6e5244f389251f8747
Salvador Aceves <salvador@aceves.mx>
2025-05-19 15:58:39 +0900

    maintance: we should also close any sequelize connections after running a set of tests

--- closeAll.js
+++ closeAll.js
@@ -1,8 +1,22 @@
 const { redisResults, queue } = require('./worker/queue');
 const { cache } = require('./cache');
+const { sequelize } = require('./models');
 
 module.exports = async () => {
+  // Close queue and redis connections
   await queue.close();
   await redisResults.quit();
   await cache.quit();
+  
+  // Close database connection if it exists and is open
+  if (sequelize && typeof sequelize.close === 'function') {
+    try {
+      await sequelize.close();
+    } catch (err) {
+      // If connection is already closed, ignore the error
+      if (!err.message.includes('Connection already closed')) {
+        console.error('Error closing sequelize connection:', err);
+      }
+    }
+  }
 };
--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,22 @@
-/* global afterAll */
+/* global beforeAll afterAll */
 
 const closeAll = require('./closeAll');
+const models = require('./models');
 
+// Setup global beforeAll and afterAll hooks
+beforeAll(async () => {
+  // Ensure database connection is established before tests
+  if (models.sequelize && typeof models.sequelize.authenticate === 'function') {
+    await models.sequelize.authenticate();
+  }
+});
+
+// Make sure all connections are properly closed after tests
 afterAll(async () => {
+  // Wait a bit to ensure all pending operations are completed
+  await global.sleep(100);
+  
+  // Then close all connections
   await closeAll();
 });
 
