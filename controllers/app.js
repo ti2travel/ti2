9a14e8de319165d24337af6167c4ef01bdce9f31
Sachin Shintre <sachin.shintre@gmail.com>
2025-09-18 16:02:32 -0500

    Add currency code setting containing list of all currencies

--- controllers/app.js
+++ controllers/app.js
@@ -62,6 +62,32 @@ const tokenTemplate = async (req, res, next) => {
     return res.json({
       template: {
         ...template,
+        currencyCode: {
+          type: 'string',
+          label: 'Currency Code',
+          description: 'Currency code to use for no rate services',
+          list: ['--Select Currency--',
+            'AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN',
+            'BAM', 'BBD', 'BDT', 'BGN', 'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BRL',
+            'BSD', 'BTN', 'BWP', 'BYN', 'BZD', 'CAD', 'CDF', 'CHF', 'CKD', 'CLP',
+            'CNY', 'COP', 'CRC', 'CUC', 'CUP', 'CVE', 'CZK', 'DJF', 'DKK', 'DOP',
+            'DZD', 'EGP', 'EHP', 'ERN', 'ETB', 'EUR', 'FJD', 'FKP', 'FOK', 'GBP',
+            'GEL', 'GGP', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ', 'GYD', 'HKD', 'HNL',
+            'HRK', 'HTG', 'HUF', 'IDR', 'ILS', 'IMP', 'INR', 'IQD', 'IRR', 'ISK',
+            'JEP', 'JMD', 'JOD', 'JPY', 'KES', 'KGS', 'KHR', 'KID', 'KMF', 'KPW',
+            'KRW', 'KWD', 'KYD', 'KZT', 'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'LYD',
+            'MAD', 'MDL', 'MGA', 'MKD', 'MMK', 'MNT', 'MOP', 'MRU', 'MUR', 'MVR',
+            'MWK', 'MXN', 'MYR', 'MZN', 'NAD', 'NGN', 'NIO', 'NOK', 'NPR', 'NZD',
+            'OMR', 'PAB', 'PEN', 'PGK', 'PHP', 'PKR', 'PLN', 'PND', 'PRB', 'PYG',
+            'QAR', 'RON', 'RSD', 'RUB', 'RWF', 'SAR', 'SBD', 'SCR', 'SDG', 'SEK',
+            'SGD', 'SHP', 'SLL', 'SLS', 'SOS', 'SRD', 'SSP', 'STN', 'SVC', 'SYP',
+            'SZL', 'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TRY', 'TTD', 'TVD', 'TWD',
+            'TZS', 'UAH', 'UGX', 'USD', 'UYU', 'UZS', 'VED', 'VES', 'VND', 'VUV',
+            'WST', 'XAF', 'XCD', 'XOF', 'XPF', 'YER', 'ZAR', 'ZMW', 'ZWB', 'ZWL',
+            'Abkhazia', 'Artsakh',
+          ],
+          value: '--Select Currency--',
+        },
         // ttlProducts and doNotCallPluginForProducts are being used at ti2 level
         // hence we should just by default allow them for all ti2 plugins
         ttlForProducts: {
